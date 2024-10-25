5ca56170cbb27974f0057f798d06ccf783b0a50d
potatochips178 <potatochips@sookmyung.ac.kr>
2024-10-25 22:20:20 +0900

    Initial commit

---
+++ src/test/java/com/jobakchoi/memebot/MemebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobakchoi.memebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
